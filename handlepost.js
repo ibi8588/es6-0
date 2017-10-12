553e4f6af2fc9bc0e19272cbba01e1a914ce46ef
ialdrareshare <ibrahim.aldridge@raregenomics.org>
2017-10-11 23:01:56 -0700

    two more solutions

---
+++ handlepost.js
@@ -0,0 +1,10 @@
+
+var posts = [
+      { id: 23, title: 'Daily JS News' },
+      { id: 52, title: 'Code Refactor City' },
+      { id: 105, title: 'The Brightest Ruby' }
+    ];
+savePost();
+function handlePosts(){
+    posts.forEach(savePost);
+}
--- sum.js
+++ sum.js
@@ -3,8 +3,12 @@ var numbers = [1,2,3,4,5,6,7];
 //create a variable to hold the sum
 var sum = 0
 //loop over the array, incrementing the sum variable
-numbers.forEach(function(number){
+
+function adder(number){
   sum += number;
-})
+}
+
+numbers.forEach(adder);
+
 // print the sum variable
 console.log(sum);
