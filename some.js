ede541a9fda19ee65c1267d39f7efb8a02335e8b
Ibrahim <ialdridge@lqdigital.com>
2018-01-10 16:59:37 -0800

    answered quiz.

--- some.js
+++ some.js
@@ -7,3 +7,13 @@ var computers = [
 computers.some(function(computer){
   return computer.ram > 16;
 });
+
+//Given an array of network objects representing network requests, assign the boolean 'true' to the variable 'inProgress' if any network request has a 'status' of 'pending'.
+
+var requests = [
+  { url: '/photos', status: 'complete' },
+  { url: '/albums', status: 'pending' },
+  { url: '/users', status: 'failed' }
+];
+
+var inProgress = requests.some( request => request.status === 'pending');
