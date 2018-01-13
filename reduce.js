846cf9bf1e81b85d2eed6cec440fb0548f285f44
Ibrahim <ialdridge@lqdigital.com>
2018-01-12 22:53:03 -0800

    created reduce file

--- reduce.js
+++ reduce.js
@@ -5,3 +5,14 @@ var sum = 0;
 numbers.reduce(function(sum, number){
   return sum + number;
 }, 0);
+
+var primaryColors = [
+  { color: 'red' },
+  { color: 'blue' },
+  { color: 'yellow' }
+];
+
+primaryColors.reduce(function(previous, primaryColor){
+  previous.push(primaryColor.color);
+  return previous
+}, []);
